18735bc8f51cf533c2448e104c5dfce02a30a304
이남곤 <nglee.dev@gmail.com>
2023-09-18 10:51:04 +0900

    feat: Redirect 되면 인가코드와 함께 백엔드에 전송

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/public", express.static(__dirname + "/public"));
 
 const server = http.createServer(app);
 const PORT = 3000;
@@ -12,8 +13,14 @@ app.get("", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/oauth/kakao/redirect", (req, res) => {
+app.get("/oauth/kakao/redirect", async (req, res) => {
   res.sendFile(__dirname + "/html/kakaoRedirect.html");
+  const code = req.query.code;
+  try {
+    const response = await axios.get(`http://localhost:8080/oauth/login/kakao?code=${code}`);
+    const data = response.data;
+    alert("로그인 성공: " + data);
+  } catch (error) {}
 });
 
 server.listen(PORT, () => {
--- public/script.js
+++ public/script.js
@@ -1,7 +1,3 @@
 document.getElementById("kakaoBtn").onclick = () => {
   window.location.href = "http://localhost:8080/oauth/kakao";
 };
-
-const popup = () => {
-  console.log("popup");
-};
